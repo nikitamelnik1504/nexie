9a760031bacec223f20f77fb1ec1065900c6e84c
skripchak <90845291+skripchak@users.noreply.github.com>
2024-10-11 23:03:11 +0000

    Fixed the processing of new messages throught web sockets

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import {WebSocketServer} from "ws";
 import Chat from "./src/Chat.js";
 import MessageRetriever from "./src/MessageRetreiver.js";
 import ProfileManager from "./src/managers/ProfileManager.js";
+import { message } from "telegraf/filters";
 
 const bot = new Telegraf('7639460431:AAFv2g2y9wdz1GEb7gORgiugyJ8qWlYHkRg');
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiODBlOGFjM2E4ZGY1NjZiZjkxYjU2MjU0NjM4NWE4NjI1NjlhMGEwMTE5OTVkZWVlZjNkMWU1YzM3ZjRjN2I1MDQ5NjFiZDA5MTgyMzkyMjIiLCJpYXQiOjE3MjcyOTI1OTcuNjExNTQ1LCJuYmYiOjE3MjcyOTI1OTcuNjExNTQ2LCJleHAiOjE3NTg4Mjg1OTcuNTk2OTYzLCJzdWIiOiIzNzUwNTE0Iiwic2NvcGVzIjpbXX0.cu3orWJhCEn2nqytQXTs7H6_7DhzlcSbZXxOTkbmkOzA_VpVRVuCrU3wtLGl-NSGhdi9yLTaE6Jo5i14NSqJTsUf3j2eWetUoHN3t-XIT_0dcHxFTUD1NvEF0JoJX6CPk05r6AOC7Dw4f10SPGVWbdVct2vJJDuvnWKdWDR8AAsvj-Uszgg8R8u80P1zh1o3OpQ5qSXsl1kHeacj4Nog1ZNKDZ6kGQ-6b1yR8bOItjW_FlIu37pfiiZNzWB6WIs_N7amRB9EAXXAl1AQxLHZHreJ0butYzn6nWIfW-2vvB5ZS8H6nDI-khpzAo4-Qomeg8qPELJmCDEoTbOdaQB-TISFATMGvAI2oYIbVDHpk0GxGGEh4RiM3A181IToypYdsxvsmSYrzgEsybekdQavuVIiVnhLcyNcZJRIfYYFj14pyk_oCwRl12yMUYylFU6q_LN7_Nj-zaEH0jAIHlRWM2gVZCCIgb_-37xgtwT6hOc_JGohhl1p_wIjW-HgJjgP-42l4JAmCcLJBKtVSS6PilGO9tPfldfqgK0Z4fHpBOgJkkpNxcqhlykf91hbU4h85eIqD1UH5bhVdr5608mN1_FC_VRqX8W_RvDskQi47_7Z0lSdj7lL6E-dZ4YJpIYfqc0BTXlXFJb0_uTSIlXFTAQFYSEs-Su7TR_ywbXFI4Y'
@@ -21,7 +22,7 @@ bot.hears('Chats', async (ctx) => {
         ctx.reply(
             `[${msg.name}] ${msg.message}`,
             Markup.inlineKeyboard([
-                Markup.button.webApp('Open chat', `https://71b7-35-205-71-138.ngrok-free.app/chat?profile_id=${msg.profileId}&name=${msg.name}`)
+                Markup.button.webApp('Open chat', `https://d259-20-61-126-212.ngrok-free.app/chat?profile_id=${msg.profileId}&name=${msg.name}`)
             ])
         );
     });
@@ -60,7 +61,9 @@ app.get('/chat', async (req, res) => {
             </div>
             
             <div class="chat-body">
-             {chat}
+             <div class="loader-container">
+                <div class="loader"></div>
+             </div>
             </div>
             
             <form class="chat-footer">
@@ -80,9 +83,23 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
     console.log('server running');
     // console.log(await ProfileManager.getProfiles(token)); //Medix Kate
-    const messageRetreiver = new MessageRetriever(token, '438410210');
-    await messageRetreiver.start();
-    await messageRetreiver.getMessageFromWebSocket();
+    // const messageRetreiver = new MessageRetriever(token, '438410210');
+    // await messageRetreiver.start().then(() => {
+    //     messageRetreiver.getMessageFromWebSocket();
+    // });
+    
+    // const messages = await messageRetreiver.getProfileMessage();
+    // const allMessages = await getArrayFromFile('./private/messages.json');
+    // console.log(messages);
+    // for (const msg of messages) {
+    //     allMessages.push({
+    //         profileId: messageRetreiver.profileId,
+    //         message: msg.message,
+    //         name: msg.name
+    //     })
+    // }
+    // await writeArrayToFile('./private/messages.json', allMessages);
+    
 })
 
 bot.launch();
@@ -90,11 +107,12 @@ bot.launch();
 const wss = new WebSocketServer({port: 8080});
 
 wss.on('connection', (ws) => {
-
     ws.on('message', async (message) => {
         const data = JSON.parse(message);
         if (data.userName && data.profileId) {
-            ws.chat = new Chat(token, data.profileId);
+            ws.chat = new Chat(token, data.profileId, (newMessage) => {
+                console.log(newMessage);
+            });
             await ws.chat.startChat(data.userName);
             const chat = await ws.chat.chat;
             ws.send(JSON.stringify({chat}));
--- src/MessageRetreiver.js
+++ src/MessageRetreiver.js
@@ -41,10 +41,10 @@ export default class MessageRetriever {
             await page.screenshot({ path: 'test.png' })
 
             return await page.evaluate(async () => {
-                const wrappers = await document.querySelectorAll('.Dialog__cont');
+                const wrappers = document.querySelectorAll('.Dialog__cont');
                 let results = [];
 
-                await wrappers.forEach(wrapper => {
+                wrappers.forEach(wrapper => {
                     if (wrapper.nextElementSibling && wrapper.nextElementSibling.classList.contains('Dialog__unread')) {
                         results.push({
                             name: wrapper.querySelector('.Dialog__name').innerText,
@@ -59,34 +59,40 @@ export default class MessageRetriever {
         }
     }
 
-    async getMessageFromWebSocket(token, profileId) {
-        for (const site of this.sitesList) {
-            const cookies = await this.cookie.getFilteredCookieByUrl(site.url);
-            this.page = await this.browser.newPage();
-            await this.page.setCookie(...cookies);
+    async getMessageFromWebSocket() {
+        try {
+            for (const site of this.sitesList) {
+                const cookies = await this.cookie.getFilteredCookieByUrl(site.url);
+                this.page = await this.browser.newPage();
+                await this.page.setCookie(...cookies);
 
-            const client = await this.page.target().createCDPSession();
-            await client.send('Network.enable');
+                this.page.on('console', async (msg) => console.log('puppeteer:', await Promise.all(msg.args().map(arg => arg.jsonValue()))));
 
-            client.on('Network.webSocketCreated', ({ requestId, url }) => {
-                console.log('WebSocket created', url);
-            });
+                const client = await this.page.target().createCDPSession();
+                await client.send('Network.enable');
 
-            client.on('Network.webSocketFrameReceived', async ({ requestId, timestamp, response }) => {
-                console.log(response.payloadData.body.message.text);
-                console.log(response.payloadData);
-                if (response.payloadData.message.text) {
-                    const messages = await getArrayFromFile('./private/messages.json');
-                    messages.push({
-                        name: response.payloadData.body.user.firstname +' '+ response.payloadData.body.user.lastname,
-                        message: response.payloadData.body.message.text,
-                        profileId: profileId
-                    });
-                    await writeArrayToFile('./private/messages.json', messages);
-                    console.log('WebSocket initialized');
-                }
-            });
-            await this.page.goto(`https://${site.url}${site.messages_url}`, { waitUntil: 'networkidle2', timeout: 60000 });
+                client.on('Network.webSocketCreated', ({ requestId, url }) => {
+                    console.log('WebSocket created', url);
+                });
+
+                client.on('Network.webSocketFrameReceived', async ({ requestId, timestamp, response }) => {
+                    const data = JSON.parse(response.payloadData);
+                    
+                    if (data.body.message) {
+                        const messages = await getArrayFromFile('./private/messages.json');
+                        messages.push({
+                            name: data.body.user.firstName +' '+ data.body.user.lastName,
+                            message: data.body.message.text,
+                            profileId: this.profileId
+                        });
+                        await writeArrayToFile('./private/messages.json', messages);
+                        console.log('WebSocket initialized');
+                    }
+                });
+                await this.page.goto(`https://${site.url}${site.messages_url}`, { waitUntil: 'networkidle2', timeout: 60000 });
+            }
+        } catch {
+            this.getMessageFromWebSocket()
         }
     }
 
