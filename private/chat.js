9fd7b78e306837faf10a926632e3effb41da0044
skripchak2 <136877778+skripchak2@users.noreply.github.com>
2024-11-05 05:54:58 +0000

    Added loader chat with file

--- private/chat.js
+++ private/chat.js
@@ -39,14 +39,6 @@ socket.onmessage = (message) => {
     }
 }
 
-fetch ('/dialogs.json')
-    .then(response => {
-
-    })
-    .then(data => {
-        console.log(data);
-    })
-
 const sendButton = document.querySelector('.send-button');
 const messageValue = document.querySelector('.chat-input');
 const form = document.querySelector('.chat-footer');
@@ -60,3 +52,49 @@ form.addEventListener('submit', (event) => {
     msg[msg.length - 1].innerText = messageValue.value;
     messageValue.value = '';
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch ('/dialogs.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error load dialog');
+        }
+        return response.json();
+    })
+    .then(dataResponse => {
+        let data = [];
+        for (const dialog of dataResponse) {
+            if (dialog.profileId !== null && dialog.dialogId === dialogId) {
+                data = dialog;
+            } else if (dialog.dialogId === null && dialog.name === userName && dialog.platform === platform) {
+                data = dialog;
+            }
+        }
+
+        if (data?.messages) {
+            let chat = '';
+            for (const message of data.messages) {
+                if (message.sender === 'inbox') {
+                    chat += `
+                        <div class="message received">
+                            <p>${message.message}</p>
+                        </div>`
+                } else {
+                    chat += `
+                    <div class="message sent">
+                        <p>${message.message}</p>
+                    </div>`
+                }
+            }
+            elementChatBody.innerHTML = chat;
+            elementChatBody.scrollBy(0, 100000);
+        } else if (data.newMessage) {
+            elementChatBody.innerHTML += `<div class="message received">
+            <p>${data.newMessage}</p>
+        </div>`;
+        }
+    })
+    .catch(err => {
+        console.error(err);
+    })
+})
