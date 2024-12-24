72e922059e4d66388e7f0e055412b20691abc730
Nikita <nikitamelnikwork@gmail.com>
2024-12-24 11:51:06 +0200

    #Add authorization status functions for Fansly and Ton.

--- bot/src/Service/Dolphin/Browser/DolphinFanslyTab.js
+++ bot/src/Service/Dolphin/Browser/DolphinFanslyTab.js
@@ -1,6 +1,55 @@
+import {timeout} from "puppeteer";
+
 class DolphinFanslyTab {
 
-  constructor(page) { 
+  dialogsEmitter = null;
+  messagesWebSocket = null;
+  browser;
+  profile;
+
+  constructor(profile, browser) {
+    this.profile = profile;
+    this.browser = browser;
+  }
+
+  static async open(profile, browser) {
+    const instance = new this(profile, browser);
+
+    // try {
+    //   const cookies = await instance.profile.exportCookies();
+    //   await instance.page.setCookie(...cookies);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    return instance;
+  }
+
+  async getAuthorizationStatus(username, login = null, password = null) {
+    const page = await this.browser.newPage();
+    await page.setViewport({width: 414, height: 896});
+
+    await page.goto(`https://fansly.com/`, {waitUntil: 'networkidle0', timeout: 60000});
+    console.log(page);
+
+    try {
+      if ((await page.$('a[href="/home"]', {timeout: 3000}) !== null)) {
+        return 0;
+      } else {
+        const profileLink = await page.$eval('div.user-account a', (el) => el.getAttribute('href'));
+        await page.goto(`https://fansly.com` + profileLink, {waitUntil: 'networkidle0', timeout: 60000});
+        const usernameElement = await page.waitForSelector('app-account-username.user-name span.user-name', { timeout: 15000 });
+        const accountUsername = await usernameElement.evaluate(el => el.textContent.replace('@', '').trim());
+
+        if (accountUsername !== username) {
+          return 2;
+        }
+
+        return 1;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
--- bot/src/Service/Dolphin/Browser/DolphinTonTab.js
+++ bot/src/Service/Dolphin/Browser/DolphinTonTab.js
@@ -1,6 +1,53 @@
 class DolphinTonTab {
 
-  constructor(page) {
+  dialogsEmitter = null;
+  messagesWebSocket = null;
+  browser;
+  profile;
+
+  constructor(profile, browser) {
+    this.profile = profile;
+    this.browser = browser;
+  }
+
+  static async open(profile, browser) {
+    const instance = new this(profile, browser);
+
+    // try {
+    //   const cookies = await instance.profile.exportCookies();
+    //   await instance.page.setCookie(...cookies);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    return instance;
+  }
+
+  async getAuthorizationStatus(username, login = null, password = null) {
+    const page = await this.browser.newPage();
+    await page.setViewport({width: 414, height: 896});
+
+    await page.goto(`https://ton.place/`, {waitUntil: 'networkidle0', timeout: 60000});
+    console.log(page);
+
+    try {
+      if ((await page.$('a[href="/dashboard"]', {timeout: 3000}) !== null)) {
+        return 0;
+      } else {
+
+        await page.click('a.UnitPhoto');
+        await page.waitForSelector('div.Profile__common_info__cont');
+        const accountUsername = await page.$eval('div.Profile__common_info__cont h1.Profile__common_info__name', (el) => el.textContent.trim());
+
+        if (accountUsername.replace(' ', '_') !== username) {
+          return 2;
+        }
+
+        return 1;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
